c832851abfe7d688914b941c84d01c7a22469607
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-06-09 08:15:58 +0900

    ESlint以外倒したぞ！！！！！！！！

--- server/repository/predictBoardRepository.ts
+++ server/repository/predictBoardRepository.ts
@@ -1,6 +1,7 @@
-/*
+import type { BoardArr} from "./boardRepository";
+import { boardRepository } from "./boardRepository";
 
-const board = boardRepository.getBoard();
+
 const direction = [
   [-1, -1],
   [-1, 0],
@@ -11,50 +12,67 @@ const direction = [
   [1, -1],
   [0, -1],
 ];
-function isValid(board: BoardArr, x: number, y: number): boolean {
-  return board[x] !== undefined && board[x][y] !== undefined && board[x][y] !== -1;
-}
+let pass = true;
+function predict(turn: number ,newBoard: BoardArr) {
+  
+  
+  const direction = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+  ];
 
-function canPlace(
-  board: BoardArr,
-  x: number,
-  y: number,
-  turn: number,
-  dx: number,
-  dy: number
-): boolean {
-  for (let c = 2; c < 7; c++) {
-    if (!isValid(board, x + dx * c, y + dy * c) || board[x + dx * c][y + dy * c] === 0) {
-      break;
-    }
-    if (board[x + dx * c][y + dy * c] === turn) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function predict(board: BoardArr, turn: number): void {
   for (let a = 0; a < 8; a++) {
     for (let b = 0; b < 8; b++) {
       let checked = false;
-      if (![1, 2].includes(board[a][b])) {
-        for (const t of direction) {
-          if (
-            isValid(board, a + t[0], b + t[1]) &&
-            board[a + t[0]][b + t[1]] !== turn &&
-            board[a + t[0]][b + t[1]] !== 0
-          ) {
-            if (canPlace(board, a, b, turn, t[0], t[1])) {
-              board[a][b] = 0;
+      if (newBoard[a][b] === 1 || newBoard[a][b] === 2) {
+        //白か黒はスキップ
+        //console.log(b, a, 'は白か黒');
+        continue;
+      }
+      for (const t of direction) {
+        if (
+          //見た方向が→
+          newBoard[a + t[0]] === undefined || //枠外ならスキップ
+          newBoard[a + t[0]][b + t[1]] === undefined || //枠外ならスキップ
+          newBoard[a + t[0]][b + t[1]] === -1 || //不可マスならスキップ
+          newBoard[a + t[0]][b + t[1]] !== turn //自分の色ならスキップ
+        ) {
+          //console.log(b, a, t, '方向はスキップ');
+          continue;
+        } else if (newBoard[a + t[0]][b + t[1]] === 0) {
+          //おけるマスならスキップ
+
+          continue;
+        } else {
+          //見た方向が自分の色じゃないなら
+          for (let c = 2; c < 7; c++) {
+            if (
+              newBoard[a + t[0] * c] === undefined || //枠外ならスキップ
+              newBoard[a + t[0] * c][b + t[1] * c] === undefined || //枠外ならスキップ
+              newBoard[a + t[0] * c][b + t[1] * c] === 0 || //おけるマスならスキップ
+              newBoard[a + t[0] * c][b + t[1] * c] === -1 //空白マスならスキップ
+            ) {
+              break;
+            } else if (newBoard[a + t[0] * c][b + t[1] * c] !== turn) {
+              //console.log(b, a, 'は置けます');
+              newBoard[a][b] = 0;
+              pass = false;
               checked = true;
               break;
             }
           }
         }
       }
-      if (!checked) {
-        board[a][b] = -1;
+
+      if (checked === false) {
+        //console.log(b, a, 'は置けません');
+        newBoard[a][b] = -1;
       }
     }
   }
@@ -62,10 +80,8 @@ function predict(board: BoardArr, turn: number): void {
 
 export const predictedBoard = {
   predictBoard: (board: BoardArr, turn: number) => {
-    predict(board, turn);
+    const newBoard = boardRepository.getBoard();
+    predict(3-turn,newBoard);
     return board;
   },
 };
-
-
-*/
