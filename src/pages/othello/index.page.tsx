c74e96340453f57566d6db74e952be207e3c0191
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-06-11 23:00:49 +0900

    個数表示とターンの色変わるようにした

--- src/pages/othello/index.page.tsx
+++ src/pages/othello/index.page.tsx
@@ -15,6 +15,7 @@ const Home = () => {
     await apiClient.board.$post({ body: { x, y } });
     await fetchBoard();
     await fetchTurn();
+    
   };
   const [user] = useAtom(userAtom);
   const [label, setLabel] = useState('');
@@ -25,9 +26,24 @@ const Home = () => {
   const [turn, setTurn] = useState<number>();
   const fetchBoard = async () => {
     const board = await apiClient.board.$get().catch(returnNull);
-    if (board !== null) setBoard(board.board);
+    console.log(board);
+    if (board !== null) setBoard(board.board);   
+    await fetchCount();
   };
-
+  const fetchCount = async () => {
+    const board = await apiClient.board.$get().catch(returnNull);
+    let black = 0;
+    let white = 0;
+    board?.board.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell === 1) black++;
+        if (cell === 2) white++;
+      });
+    }
+    );
+    document.getElementsByClassName(styles.black)[0].innerHTML = `黒:${black}個`;
+    document.getElementsByClassName(styles.white)[0].innerHTML = `白:${white}個`;
+  } 
   const fetchTurn = async () => {
     const turn = await apiClient.turn.$get().catch(returnNull);
     if (turn !== null) setTurn(turn.turn);
@@ -52,8 +68,15 @@ const Home = () => {
       <BasicHeader user={user} />
       <div className={styles.container}>
         <div className={styles.pass} />
-        <div className={styles.turn}>
-          {turn === 1 ? '黒のターン' : '白のターン'}
+        <div className={styles.turn}
+        style =
+        {{
+          color: turn === 1 ? '#000000e4' : '#fffffff2',
+        }}
+        >
+          {turn === 1 ? '黒' : '白'}
+          <span style = {{color: '#000000e4'}}>のターン</span>
+
         </div>
         <div className={styles.board}>
           {board.map((row, y) =>
